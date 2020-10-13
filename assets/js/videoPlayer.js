9af4cac33832abd15c7b49485d358b6183d0c68c
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-10-13 17:32:25 +0900

    full screen control

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayBtn");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,9 +24,24 @@ function handleVolumeClick() {
   }
 }
 
+function outFullScreen() {
+  document.exitFullscreen();
+  fullScreenBtn.innerHTML = '<i class= "fas fa-expand"></i>';
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  fullScreenBtn.removeEventListener("click", outFullScreen);
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullScreenBtn.innerHTML = '<i class= "fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", outFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
