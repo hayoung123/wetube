a5e3d7bc0fcfe0eed5f6438c5a8a7eb8e11c74a1
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-10-28 23:43:38 +0900

    complete hover div box of video thumbnail

--- src/assets/js/videoBlock.js
+++ src/assets/js/videoBlock.js
@@ -1,17 +1,30 @@
+import getBlobDuration from "get-blob-duration";
 const { formatDate } = require("./videoPlayer");
 
 const videoWrapper = document.querySelectorAll(".video__wrapper");
+const videoThumbnail = document.querySelectorAll("#jsVideoThumbnail");
 const videoCover = document.querySelectorAll("#jsVideoCover");
 
 const handleCover = (index) => {
   videoCover[index].classList.add("showing");
+  videoCover[index].classList.remove("hidden");
 };
 
 const handleCoverOut = (index) => {
   videoCover[index].classList.remove("showing");
+  videoCover[index].classList.add("hidden");
 };
 
-const setCoverTime = () => {};
+const setCoverTime = async (index) => {
+  const blob = await fetch(videoThumbnail[index].src).then((response) =>
+    response.blob()
+  );
+  const duration = await getBlobDuration(blob);
+  const totalTimeString = formatDate(duration);
+  const span = document.createElement("span");
+  span.innerText = totalTimeString;
+  videoCover[index].appendChild(span);
+};
 
 function init() {
   videoWrapper.forEach((item, index) => {
@@ -22,9 +35,12 @@ function init() {
       handleCoverOut(index);
     });
   });
+
   document.onreadystatechange = function () {
     if (document.readyState === "complete") {
-      setCoverTime();
+      videoWrapper.forEach((item, index) => {
+        setCoverTime(index);
+      });
     }
   };
 }
